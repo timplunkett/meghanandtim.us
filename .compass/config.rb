ba906fd0be01c4542c05abbbbd24edf85cc33b8f
Tim Plunkett <git@plnktt.com>
2014-03-29 18:12:37 -0700

    Downgrade sass and compass.

---
+++ .compass/config.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'sass'
+require 'compass'
+
+http_path = '/'
+css_dir = 'stylesheets'
+sass_dir = '_sass'
+fonts_dir = 'fonts'
+images_dir = 'images'
+javascripts_dir = 'javascripts'
+
+line_comments = false
+preferred_syntax = :sass
+asset_cache_buster :none
--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source "https://rubygems.org"
 
 gem 'github-pages'
-gem 'sass', "3.3.0"
-gem "compass", "~> 1.0.0.alpha.13"
-gem "breakpoint", "~> 2.4.0"
+gem 'sass', "~> 3.2.17"
+gem "compass", "~> 0.12"
